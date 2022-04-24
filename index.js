3a9005b170eb6b9ddbe9cd268109ad3c7e4b411b
Akhmed Umarov <ahmad.umarov7@gmail.com>
2022-04-24 17:36:13 +0300

    delete lishka

--- index.js
+++ index.js
@@ -10,19 +10,3 @@ if (PosBut > 1.95 * document.documentElement.clientHeight){
 }
 
 })
-
-
-// window.addEventListener('scroll', ()=> { 
-//    let PosBut = document.documentElement.getBoundingClientRect().bottom
-//    let fix = document.querySelector(`header`); 
-//    if (PosBut <  1.8 * document.documentElement.clientHeight) { 
-//       fix.classList.add('b-show'); 
-//    }
-
-// if (PosBut > 1.9 * document.documentElement.clientHeight){ 
-   
-//    fix.classList.add('b-clear'); 
-
-// }
-
-// });
